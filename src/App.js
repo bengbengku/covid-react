9cfc2e3deaf47bf3fde7a5f4024c3b1fe0ff5553
bengbengku <anggiatbenget70@gmail.com>
2020-10-12 22:54:25 +0700

    ADD: Mendaptkan data API menggunakan useState dan useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import { FormControl, MenuItem, Select } from '@material-ui/core';
-import React from 'react';
-import './App.css';
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          console.log(countries);
+          setCountries(countries);
+
+        });
+    };
+
+    getCountriesData();
+
+  }, []);
+
   return (
     <div className="app">
-      <h1>Covid-19 Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="Worldwide">Worldwide</MenuItem>
-          <MenuItem value="Worldwide">Option II</MenuItem>
-          <MenuItem value="Worldwide">Option III</MenuItem>
-        </Select>
-      </FormControl>
-
-
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
