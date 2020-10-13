fd84dcace67d68d750b626c748bfc64e47f67215
bengbengku <anggiatbenget70@gmail.com>
2020-10-14 01:34:18 +0700

    ADD: Setup props casesType ketika diklik

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, total, ...props }) {
   return (
-    <Card className="infoBox">
-        <CardContent>
-            <Typography className="infoBox__title" color="textSecondary">
-                {title}
-            </Typography>
-                <h2 className="infoBox__cases">{cases}</h2>
-            <Typography className="infoBox__total" color="textSecondary">
-                {total} Total
-            </Typography>    
-        </CardContent>
+    <Card onClick={props.onClick} className="infoBox">
+      <CardContent>
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+        <h2 className="infoBox__cases">{cases}</h2>
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
